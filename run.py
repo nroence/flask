a303188e7d3ebd1e93c104bbf93d39f616464b93
Van Rodolf Suliva <vanrodolf1@gmail.com>
2025-06-07 19:51:54 +0800

    Fix: Add Procfile and use PORT env var

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-#flask-analytics/run.py
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
